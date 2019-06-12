13276e1400789ada2b834d11fb2de8078d8c6a58
Ranndy360 <ranndy360@gmaill.com>
2019-06-12 09:53:30 -0600

    spring security - jwt

--- src/main/java/sv/com/core/meeting/service/JwtTokenService.java
+++ src/main/java/sv/com/core/meeting/service/JwtTokenService.java
@@ -2,17 +2,22 @@ package sv.com.core.meeting.service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jdk.nashorn.internal.objects.annotations.Constructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 import java.util.HashMap;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+import java.util.function.Function;
 
 @Component
 public class JwtTokenService {
 
+    @Value("${jwt.secret}")
     private String secret;
-
+    @Value("${jwt.expiration}")
     private Long expiration;
 
     public JwtTokenService(@Value("${jwt.secret}") String secret,
@@ -37,4 +42,39 @@ public class JwtTokenService {
     private Date calculateExpirationDate(Date createdDate) {
         return new Date(createdDate.getTime() + expiration * 10000);
     }
+
+    @Autowired
+    public JwtTokenService(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenNotExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.after(new Date());
+    }
+
+    public Optional<Boolean> validateToken(String token) {
+        return  isTokenNotExpired(token) ? Optional.of(Boolean.TRUE) : Optional.empty();
+    }
+
 }
